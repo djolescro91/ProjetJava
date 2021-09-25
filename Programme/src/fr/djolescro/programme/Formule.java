06555162bf4f6ee3a4efaaa6b66e1cd7e8666f06
Grégory Allard <allard.gregory@hotmail.com>
2021-09-26 00:39:01 +0100

    Ajout d'espace pour meilleur lisibilité

--- Programme/src/fr/djolescro/programme/Formule.java
+++ Programme/src/fr/djolescro/programme/Formule.java
@@ -3,9 +3,11 @@ package fr.djolescro.programme;
 public class Formule {
 	// Formule Aire d'un trapèze
 	public static double aireTrapeze(double grandeBase, double petiteBase, double hauteur) {
+
 		double formule = ((grandeBase + petiteBase) * hauteur) / 2;
 		return formule;
 	}
+	
 	// Formule volume d'une Sphère ou boule
 		public static double volumeSphere(double base, double hauteur) {
 			double formule = (1 / 3 * base * hauteur);
