934ede3911236039338c25f410b096af17e4c321
Grégory Allard <allard.gregory@hotmail.com>
2021-09-30 19:20:25 +0100

    rien de special

--- Programme/src/fr/djolescro/programme/Main.java
+++ Programme/src/fr/djolescro/programme/Main.java
@@ -1,16 +1,31 @@
 package fr.djolescro.programme;
 
+import java.util.Arrays;
+
 public class Main {
 
 	public static void main(String[] args) {
 
-		// Variables
+		// Variables pour la formule aire du trapeze
+		
 		double grandeBase = 20;
 		double petiteBase = 15;
 		double hauteur = 7;
 
 		// Affichage
-		System.out.println("L'aire du trapeze est égal a : " + String.format("%.2f", Formule.aireTrapeze(grandeBase, petiteBase, hauteur)));
+		
+		// System.out.println("L'aire du trapeze est égal a : " + String.format("%.2f", Formule.aireTrapeze(grandeBase, petiteBase, hauteur)));
+		
+		// Exercices
+		
+		
+		String managers[] = new String[] {"Jennifer", "Aurélien", "Bakari", "Gianni"};
+		
+		Arrays.sort(managers);
+		
+		for(String manager : managers) {
+			System.out.println("Nous avons comme manager " + manager);
+		}
 	}
-
+	
 }
